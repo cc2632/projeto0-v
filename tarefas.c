8d5130c3e0d7cff80b80aa57170fa3e0271d1530
cc2632 <145609640+cc2632@users.noreply.github.com>
2024-03-20 19:50:35 +0000

    implementacao da criacao das tarefas

--- tarefas.c
+++ tarefas.c
@@ -2,7 +2,23 @@
 #include <stdio.h>
 
 Erro criar(Tarefa t[], int *pos) {
-  printf("funcao criar tarefa");
+  if (*pos >= TOTAL)
+    return MAX_TAREFAS;
+
+  printf("Entre com uma prioridade: ");
+  scanf("%d", &t[*pos].prioridade);
+  clearBuffer();
+
+  printf("Entre com a descricao: ");
+  fgets(t[*pos].descricao, 300, stdin);
+  clearBuffer();
+
+  printf("Entre com a categoria: ");
+  fgets(t[*pos].categoria, 100, stdin);
+
+  printTarefa(t[*pos]);
+  *pos = *pos + 1;
+
   return OK;
 }
 Erro deletar(Tarefa t[], int *pos) {
@@ -20,4 +36,15 @@ Erro salvar(Tarefa t[], int pos, int tamanho) {
 Erro carregar(Tarefa t[], int *pos, int tamanho) {
   printf("funcao carregar tarefas");
   return OK;
+}
+
+void printTarefa(Tarefa t) {
+  printf("\nPrioridade: %d\tCategoria: %s", t.prioridade, t.categoria);
+  printf("Descricao: %s\n", t.descricao);
+}
+
+void clearBuffer() {
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF) {
+  }
 }
